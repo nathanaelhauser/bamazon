d1e6d86061714c353b33f3f8b17377d1f2a9f966
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-07 23:52:08 -0800

    Added displayData function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
-const table = require('table')
+const { table } = require('table')
+const chalk = require('chalk')
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
@@ -9,13 +10,51 @@ const db = mysql.createConnection({
   database: 'bamazon'
 })
 
-const start = _ => {
-  db.query('SELECT * FROM products', (e, data) => {
-    if (e) {
-      console.log(e)
-    }
-    console.log(data)
-    // displayData(data)
+async function start() {
+  let response = await new Promise((resolve, reject) => {
+    db.query('SELECT * FROM products', (e, data) => {
+      if (e) {
+        reject(e)
+      }
+      resolve(data)
+    })
   })
 
+  return response
 }
+
+// const start = _ => {
+//   db.query('SELECT * FROM products', (e, data) => {
+//     if (e) {
+//       console.log(e)
+//     }
+//     // console.log(data)
+//     displayData(data)
+//     db.end()
+//   })
+
+// }
+
+const displayData = data => {
+  const products = data.reduce(
+    (displayTable, { item_id, product_name, department_name, price, stock_quantity }) => {
+      displayTable.push([item_id, product_name, department_name, price, stock_quantity])
+      return displayTable
+    }, [[
+      chalk.red('ID'),
+      chalk.red('NAME'),
+      chalk.red('DEPARTMENT'),
+      chalk.red('PRICE'),
+      chalk.red('STOCK')
+    ]]
+  )
+
+  console.log(table(products))
+}
+
+
+start()
+  .then(data => {
+    displayData(data)
+  })
+  .catch(e => console.log(e))
