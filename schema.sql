5ce61958485a490ccafb559c5012de948e732c70
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-08 02:39:20 -0800

    Added departments table to sql files

--- schema.sql
+++ schema.sql
@@ -8,5 +8,14 @@ CREATE TABLE products(
   department_name VARCHAR(255) NOT NULL,
   price FLOAT(10,2) NOT NULL,
   stock_quantity INT(10) NOT NULL,
+  product_sales FLOAT(16, 2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY(item_id)
+);
+
+USE bamazon;
+CREATE TABLE departments(
+  department_id INT(10) AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(255) NOT NULL,
+  over_head_costs INT(10) NOT NULL,
+  PRIMARY KEY(department_id)
 );
--- seed.sql
+++ seed.sql
@@ -3,10 +3,24 @@ INSERT INTO products(product_name, department_name, price, stock_quantity)
   VALUES ('Banana Waterpods', 'Electronics', 8399.99, 100),
          ('Samurai Blender', 'Appliances', 56.88, 13),
          ('Fucci Purse', 'Clothing', 1.99, 5000),
-         ('Diamond Necklace', 'Jewelery', 640.49, 7),
+         ('Diamond Necklace', 'Jewelry', 640.49, 7),
          ('6.5ft Nail', 'Hardware', 999999.99, 10),
          ('Classic Warfare: Stabbing 2.0', 'Video Games', 56.99, 30),
          ('Fusion Nuclear Bomb', 'Cleaning Supplies', 100.00, 50),
          ('Hadrion Collider', 'Industrial', 3.50, 42),
          ('Greenland', 'Land', 1099.99, 2),
          ('Lightning in a Bottle', 'Novelty Gifts', 199.99, 238);
+
+USE bamazon;
+INSERT INTO departments(department_name, over_head_costs)
+  VALUES ('Electronics', 1000),
+         ('Appliances', 1000),
+         ('Clothing', 1000),
+         ('Jewelry', 1000),
+         ('Hardware', 1000),
+         ('Video Games', 1000),
+         ('Cleaning Supplies', 1000),
+         ('Industrial', 1000),
+         ('Land', 1000),
+         ('Novelty Gifts', 1000),
+         ('Aircraft', 1000);
