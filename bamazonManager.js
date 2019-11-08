91889d957fff9cd0c00a5e587faedfc18985d863
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-08 01:04:09 -0800

    Added viewAll function to manager js

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,15 +20,53 @@ const start = _ => {
     .then(({ action }) => {
       switch (action) {
         case 'View Products for Sale':
+          viewAll()
           break
         case 'View Low Inventory':
+          viewLow()
           break
         case 'Add to Inventory':
+          addInventory()
           break
         case 'Add New Product':
+          addProduct()
           break
         default:
+          console.log('You do not have permission to be here!')
           break
       }
     })
+    .catch(e => console.log(e))
 }
+
+const viewAll = _ => {
+  db.query('SELECT * FROM products', (e, data) => {
+    const display = data.reduce((productsArr, product) => {
+      productsArr.push([product.item_id, product.product_name,
+      product.department_name, product.price.toFixed(2), product.stock_quantity])
+      return productsArr
+    }, [[
+      chalk.green('ID'),
+      chalk.green('NAME'),
+      chalk.green('DEPARTMENT'),
+      chalk.green('PRICE'),
+      chalk.green('STOCK')
+    ]])
+
+    const config = {
+      columns: {
+        3: {
+          alignment: 'right'
+        },
+        4: {
+          alignment: 'right'
+        }
+      }
+    }
+
+    console.log(table(display, config))
+    db.end()
+  })
+}
+
+start()
